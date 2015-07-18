224ede2290c9ff8bb5937eb7a29103c8501a05bb
cole3 <cole3@cole3-Lenovo-G460.(none)>
2015-07-18 10:31:51 +0800

    add swim code

--- stm8_swim.c
+++ stm8_swim.c
@@ -1,106 +1,33 @@
-#include <linux/module.h>
-#include <linux/kernel.h>
-#include <linux/fs.h>
-#include <linux/init.h>
-#include <linux/delay.h>
-#include <linux/poll.h>
-#include <asm/irq.h>
-#include <asm/io.h>
-#include <linux/interrupt.h>
-#include <asm/uaccess.h>
-#include <mach/hardware.h>
-#include <plat/regs-timer.h>
-#include <mach/regs-irq.h>
-#include <asm/mach/time.h>
-#include <linux/clk.h>
-#include <linux/cdev.h>
-#include <linux/device.h>
-#include <linux/miscdevice.h>
+#include "stm8_swim.h"
 
-#include <mach/map.h>
-#include <mach/regs-clock.h>
-#include <mach/regs-gpio.h>
+/********************************************************
+ * demo:
+ *     host               target
+ * -------------       ------------
+ * |       rst |>----->| rst      |
+ * |   swim_in |<----+-| swim     |
+ * |  swim_out |>---/  |          |
+ * -------------       ------------
+ *******************************************************/
 
-#include <plat/gpio-cfg.h>
-#include <mach/gpio-bank-e.h>
-#include <mach/gpio-bank-f.h>
-#include <mach/gpio-bank-k.h>
 
-#define DEVICE_NAME     "swim"
+#define SWIM_CMD_LEN                    3
+#define SWIM_CMD_SRST                   0x00
+#define SWIM_CMD_ROTF                   0x01
+#define SWIM_CMD_WOTF                   0x02
+#define SWIM_MAX_RESEND_CNT             20
 
-#define swim_IOCTL_SET_FREQ		1
-#define swim_IOCTL_STOP			0
 
-static struct semaphore lock;
+unsigned int return_line;
+static swim_input input, *p_input = &input;
+static swim_method method, *p_method = &method;
 
-/* freq:  pclk/50/16/65536 ~ pclk/50/16 
-  * if pclk = 50MHz, freq is 1Hz to 62500Hz
-  * human ear : 20Hz~ 20000Hz
-  */
-static void swim_ndelay(unsigned long ns)
+static void swim_ndelay(unsigned int ns)
 {
-	unsigned long tcon;
-	unsigned long tcnt;
-	unsigned long tcfg1;
-	unsigned long tcfg0;
-	unsigned long tint_cstat;
-
-	struct clk *clk_p;
-	unsigned long pclk;
-
-	unsigned tmp;
-
-#if 0 // PWM IO
-	tmp = readl(S3C64XX_GPFCON);
-	tmp &= ~(0x3U << 28);
-	tmp |=  (0x2U << 28);
-	writel(tmp, S3C64XX_GPFCON);
-#endif
-
-	tcon = __raw_readl(S3C_TCON);
-	tcfg1 = __raw_readl(S3C_TCFG1);
-	tcfg0 = __raw_readl(S3C_TCFG0);
-	tint_cstat = __raw_readl(S3C_TINT_CSTAT);
-
-	//prescaler = 0
-	tcfg0 &= ~S3C_TCFG_PRESCALER0_MASK;
-	//tcfg0 |= (50 - 1); // add
-
-	//mux = 1/1
-	tcfg1 &= ~S3C_TCFG1_MUX0_MASK;
-	tcfg1 |= S3C_TCFG1_MUX0_DIV1; // add
-
-	__raw_writel(tcfg1, S3C_TCFG1);
-	__raw_writel(tcfg0, S3C_TCFG0);
-
-	clk_p = clk_get(NULL, "pclk");
-	pclk  = clk_get_rate(clk_p);
-    //printk("pclk = %u\n", (unsigned int)pclk);
-	tcnt  = ns * (pclk / 100000) / 10000;
-    //printk("tcnt = %u\n", (unsigned int)tcnt);	
-	__raw_writel(tcnt, S3C_TCNTB(0));
-	//__raw_writel(tcnt/2, S3C_TCMPB(0)); // add
-
-	tint_cstat &= ~1; 
-    tint_cstat |= (1<<5);
-	__raw_writel(tint_cstat, S3C_TINT_CSTAT);
-
-    tcon &= ~0x1f;
-    tcon |= 0xb;		//update TCNTB0, start timer 0
-    __raw_writel(tcon, S3C_TCON);
-
-    tcon &= ~2;			//clear manual update bit
-    __raw_writel(tcon, S3C_TCON);
-
-    //printk("S3C_TINT_TCNTO = %d\n", __raw_readl(S3C_TIMERREG(0x14)));
-    //printk("S3C_TINT_TCNTO = %d\n", __raw_readl(S3C_TIMERREG(0x14)));
-
-    //printk("before S3C_TINT_CSTAT 0x%X\n", __raw_readl(S3C_TINT_CSTAT));
-    while (!(__raw_readl(S3C_TINT_CSTAT) & (1<<5)));
-    //printk("after S3C_TINT_CSTAT 0x%X\n", __raw_readl(S3C_TINT_CSTAT));
+    p_input->ndelay(ns);
 }
 
-static swim_udelay(unsigned int us)
+static void swim_udelay(unsigned int us)
 {
     while (us--)
     {
@@ -108,110 +35,373 @@ static swim_udelay(unsigned int us)
     }
 }
 
-static swim_mdelay(unsigned int ms)
+static void swim_mdelay(unsigned int ms)
 {
     while (ms--)
     {
-        swim_ndelay(1000000);
+        swim_udelay(1000);
     }
 }
 
-static void swim_init_io(void)
+static void swim_send_bit(unsigned char bit)
 {
+    // low speed
+    if (bit)
+    {
+        p_input->io_set(SWIM, LOW);
+        swim_ndelay(250); // 2*(1/8M) = 250ns
+        p_input->io_set(SWIM, HIGH);
+        swim_ndelay(2500); // 20*(1/8M) = 2500ns
+    }
+    else
+    {
+        p_input->io_set(SWIM, LOW);
+        swim_ndelay(2500); // 2*(1/8M) = 250ns
+        p_input->io_set(SWIM, HIGH);
+        swim_ndelay(250); // 20*(1/8M) = 2500ns
+    }
+}
+
+static char swim_rvc_bit(void)
+{
+    unsigned int i;
+    unsigned char p = 0, m = 1;
     
-}
+    // low speed
+#define ACK_CHK_TIMEOUT 1000
+    for (i=0; i<ACK_CHK_TIMEOUT; i++)
+    {
+        swim_ndelay(125);
+        if (p_input->io_get(SWIM) == LOW)
+        {
+            m = 0;
+            p++;
+        }
 
-void swim_stop( void )
-{
-	unsigned tmp;
-	tmp = readl(S3C64XX_GPFCON);
-	tmp &= ~(0x3U << 28);
-	writel(tmp, S3C64XX_GPFCON);
-}
-
-static int s3c64xx_swim_open(struct inode *inode, struct file *file)
-{
-	if (!down_trylock(&lock))
-		return 0;
-	else
-		return -EBUSY;
-}
-
-
-static int s3c64xx_swim_close(struct inode *inode, struct file *file)
-{
-	up(&lock);
-	return 0;
-}
-
-
-static long s3c64xx_swim_ioctl(struct file *filep, unsigned int cmd, unsigned long arg)
-{
-	switch (cmd) {
-		case swim_IOCTL_SET_FREQ:
-			if (arg == 0)
-				return -EINVAL;
-			//swim_set_freq(arg);
-			break;
-
-		case swim_IOCTL_STOP:
-		default:
-			swim_stop();
-			break;
-	}
-
-	return 0;
-}
-
-
-static struct file_operations dev_fops = {
-    .owner			= THIS_MODULE,
-    .open			= s3c64xx_swim_open,
-    .release		= s3c64xx_swim_close, 
-    .unlocked_ioctl	= s3c64xx_swim_ioctl,
-};
-
-static struct miscdevice misc = {
-	.name = DEVICE_NAME,
-	.fops = &dev_fops,
-};
-
-
-static int __init dev_init(void)
-{
-	int ret;
-    struct clk   *timer_clock;
-
-	init_MUTEX(&lock);
-	ret = misc_register(&misc);
-
-    timer_clock = clk_get(NULL, "timers");
-    if (!timer_clock) {
-        printk(KERN_ERR "failed to get adc clock source\n");
-        return -ENOENT;
+        if (m == 0 && p_input->io_get(SWIM) == HIGH)
+        {   
+            return (p <= 8) ? 1 : 0;
+        }
     }
-    clk_enable(timer_clock);
-
-
-    local_irq_disable();
-    printk("start delay\n");
-    swim_ndelay(1250);
-    printk("end delay\n");
-    swim_mdelay(30000);
-    printk("end delay\n");
-    local_irq_enable();
-
-	printk (DEVICE_NAME"\tinitialized\n");
-    	return ret;
+    return -1;
 }
 
-static void __exit dev_exit(void)
+static void swim_send_ack(unsigned char ack)
 {
-	misc_deregister(&misc);
+    swim_ndelay(2750);
+    swim_send_bit(ack);
 }
 
-module_init(dev_init);
-module_exit(dev_exit);
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Cole3");
-MODULE_DESCRIPTION("S3C6410 SWIM Driver");
+static char swim_rvc_ack(void)
+{
+    return swim_rvc_bit();
+}
+
+
+static swim_ret swim_send_unit(unsigned char data, unsigned char len, unsigned int retry_cnt)
+{
+    char i;
+    unsigned char p, m;
+    char ack = 0;
+
+    swim_ndelay(2750);
+
+    do {
+        swim_send_bit(0);
+        p = 0;
+        for (i=len-1; i>=0; i--)
+        {
+            m =(data >> i) & 1;
+            swim_send_bit(m);
+            p += m;
+        }
+        // parity bit
+        swim_send_bit((p & 1));
+        ack = swim_rvc_ack();
+        if (ack == -1)
+        {
+            return SWIM_TIMEOUT;
+        }
+    } while (!ack && retry_cnt--);
+
+    return ack ? SWIM_OK : SWIM_FAIL;
+}
+
+static swim_ret swim_rcv_unit(unsigned char *data, unsigned char len)
+{
+    unsigned int i;
+    unsigned char s = 0, m = 0, p = 0, cp = 0;
+    char ack = 0;
+
+    for (i=0; i<len+2; i++)
+    {
+        ack = swim_rvc_bit();
+        if (ack == -1)
+        {
+            return SWIM_TIMEOUT;
+        }
+        if (i == 0)
+        {
+            s = ack;
+        }
+        else if (i == len + 1)
+        {
+            p = ack;
+        }
+        else
+        {
+            m <<= 1;
+            m |= ack;
+            cp += ack;
+        }
+    }
+
+    if (s == 1 && p == (cp & 1))
+    {
+        ack = 1;
+    }
+
+    swim_send_ack(ack);
+
+    return ack ? SWIM_OK : SWIM_FAIL;
+}
+
+
+static swim_ret swim_reset(void)
+{
+    return swim_send_unit(SWIM_CMD_SRST, SWIM_CMD_LEN, SWIM_MAX_RESEND_CNT);
+}
+
+static swim_ret swim_read(unsigned int addr, unsigned char *buf, unsigned int size)
+{
+    unsigned char cur_len, i;
+    unsigned int cur_addr = addr;
+    swim_ret ret = SWIM_OK;
+
+    if (!buf)
+    {
+        return SWIM_FAIL;
+    }
+
+    while (size)
+    {
+        cur_len = (size > 255) ? 255 : size;
+
+        ret = swim_send_unit(SWIM_CMD_ROTF, SWIM_CMD_LEN, SWIM_MAX_RESEND_CNT);
+        if (ret)
+        {
+            return_line = __LINE__;
+            break;
+        }
+        ret = swim_send_unit(cur_len, 8, 0);
+        if (ret)
+        {
+            return_line = __LINE__;
+            break;
+        }
+        ret = swim_send_unit((cur_addr >> 16) & 0xFF, 8, 0);
+        if (ret)
+        {
+            return_line = __LINE__;
+            break;
+        }
+        ret = swim_send_unit((cur_addr >> 8) & 0xFF, 8, 0);
+        if (ret)
+        {
+            return_line = __LINE__;
+            break;
+        }
+        ret = swim_send_unit((cur_addr >> 0) & 0xFF, 8, 0);
+        if (ret)
+        {
+            return_line = __LINE__;
+            break;
+        }
+        for (i = 0; i < cur_len; i++)
+        {
+            ret = swim_rcv_unit(buf++, 8);
+            if (ret)
+            {
+                return_line = __LINE__;
+                break;
+            }
+        }
+        if (ret)
+        {
+            break;
+        }
+
+        cur_addr += cur_len;
+        size -= cur_len;
+    }
+
+    return ret;
+}
+
+static swim_ret swim_write(unsigned int addr, unsigned char *buf, unsigned int size)
+{
+    unsigned char cur_len, i;
+    unsigned int cur_addr = addr;
+    swim_ret ret = SWIM_OK;
+
+    if (!buf)
+    {
+        return SWIM_FAIL;
+    }
+
+    while (size)
+    {
+        cur_len = (size > 255) ? 255 : size;
+
+        ret = swim_send_unit(SWIM_CMD_WOTF, SWIM_CMD_LEN, SWIM_MAX_RESEND_CNT);
+        if (ret)
+        {
+            return_line = __LINE__;
+            break;
+        }
+        ret = swim_send_unit(cur_len, 8, 0);
+        if (ret)
+        {
+            return_line = __LINE__;
+            break;
+        }
+        ret = swim_send_unit((cur_addr >> 16) & 0xFF, 8, 0);
+        if (ret)
+        {
+            return_line = __LINE__;
+            break;
+        }
+        ret = swim_send_unit((cur_addr >> 8) & 0xFF, 8, 0);
+        if (ret)
+        {
+            return_line = __LINE__;
+            break;
+        }
+        ret = swim_send_unit((cur_addr >> 0) & 0xFF, 8, 0);
+        if (ret)
+        {
+            return_line = __LINE__;
+            break;
+        }
+        for (i = 0; i < cur_len; i++)
+        {
+            ret = swim_send_unit(*buf++, 8, SWIM_MAX_RESEND_CNT);
+            if (ret)
+            {
+                return_line = __LINE__;
+                break;
+            }
+        }
+        if (ret)
+        {
+            break;
+        }
+
+        cur_addr += cur_len;
+        size -= cur_len;
+    }
+
+    return ret;
+}
+
+static swim_ret swim_entry(void)
+{
+    unsigned char i, m;
+    swim_ret ret = SWIM_FAIL;
+    unsigned char buf[1];
+
+    p_input->io_set(SWIM, HIGH);
+    p_input->io_set(RST, HIGH);
+    swim_mdelay(50);
+    p_input->io_set(RST, LOW);
+    swim_mdelay(10);
+    p_input->io_set(SWIM, LOW);
+    swim_udelay(100);
+
+    for (i=0; i<4; i++)
+    {
+        p_input->io_set(SWIM, HIGH);
+        swim_udelay(500);
+        p_input->io_set(SWIM, LOW);
+        swim_udelay(500);
+    }
+    for (i=0; i<4; i++)
+    {
+        p_input->io_set(SWIM, HIGH);
+        swim_udelay(250);
+        p_input->io_set(SWIM, LOW);
+        swim_udelay(250);
+    }
+
+    p_input->io_set(SWIM, HIGH);
+    m = 1;
+
+#define RST_CHK_TIMEOUT 1000
+    // about 16 us
+    for (i=0; i<RST_CHK_TIMEOUT; i++)
+    {
+        swim_ndelay(500);
+        if (p_input->io_get(SWIM) == LOW)
+        {
+            m = 0;
+        }
+
+        if (m == 0 && p_input->io_get(SWIM) == HIGH)
+        {
+            ret = SWIM_OK;
+        }
+    }
+
+    if (ret)
+    {
+        return_line = __LINE__;
+        return ret;
+    }
+
+    swim_mdelay(10);
+    ret = swim_reset();
+    if (ret)
+    {
+        return_line = __LINE__;
+        return ret;
+    }
+
+    swim_mdelay(30);
+    buf[0]=0xA0;
+    ret = swim_write(0x00007F80, buf, 1); 
+    if (ret)
+    {
+        return_line = __LINE__;
+        return ret;
+    }
+
+    swim_mdelay(10);
+    p_input->io_set(RST, HIGH);
+    swim_mdelay(10);
+    
+
+    return ret;
+}
+
+
+swim_method *swim_register(swim_input *swim_input)
+{
+    if (!swim_input)
+    {
+        return 0;
+    }
+
+    input = *swim_input;
+
+    p_method->entry = swim_entry;
+    p_method->reset = swim_reset;
+    p_method->read = swim_read;
+    p_method->write = swim_write;
+
+    return p_method;
+}
+
+void swim_unregister(swim_method *method)
+{
+
+}
